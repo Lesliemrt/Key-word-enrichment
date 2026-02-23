5d71c2c2d63349fdc54c376752d6f49c54295b80
Muhammad Osama Nusrat <45511078+mnusrat786@users.noreply.github.com>
2026-02-24 01:08:21 +0500

    Update configuration with new models and HF_TOKEN
    Added new model paths and HF_TOKEN for authentication.

--- src/utils/config.py
+++ src/utils/config.py
@@ -5,6 +5,7 @@ from src import taxonerd
 RANDOM_STATE = 42
 
 API_KEY="1nCRukeZXLfBrLYxFpnoVhSYcIXLvTnq"
+HF_TOKEN = "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"
 
 CSV_PATH = 'data/Data_cleaned.csv'
 EXTENDED_CSV_PATH = 'data/Data_extended.csv'
@@ -12,11 +13,25 @@ MODELS = {
     'rulebased',
     'taxonerd',
     'scibert',
+    'biodivbert',
+    'openmed',
+    'llamainstruct',
     'rulebased-scibert',
     'rulebased-taxonerd',
+    'rulebased-biodivbert',
+    'rulebased-openmed',
+    'rulebased-llamainstruct',
     'mistral'
 }
 
+
 scibert_model_path = "allenai/scibert_scivocab_uncased"
+biodivbert_model_path = "NoYo25/BiodivBERT"
+openmed_model_path = "OpenMed/OpenMed-NER-SpeciesDetect-BioMed-109M"
+llama_model_path = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+
+
+biodivbert_checkpoint_dir = "checkpoints_biodivbert_split"
+openmed_checkpoint_dir = "checkpoints_openmed_split"
 
 
